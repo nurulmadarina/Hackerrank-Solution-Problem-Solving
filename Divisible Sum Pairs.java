e231d54f59b98205af127789600aabc8f6265f9a
nurulmadarina <nurul.lailani.m@mail.ugm.ac.id>
2020-04-13 20:45:55 +0700

    add solution Divisible Sum Pairs

---
+++ Divisible Sum Pairs.java
@@ -0,0 +1,50 @@
+public class Solution {
+
+    // Complete the divisibleSumPairs function below.
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        int sum = 0;
+        int jum = 0 ;
+        for (int i = 0; i < n; i ++){
+            for (int j = i; j < n; j++){
+                 sum = ar[i] + ar[j];
+                   if ((sum % k)== 0 && i !=j){
+                    jum +=1;
+                }
+            }
+            
+        }
+        return jum;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = divisibleSumPairs(n, k, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
